859fcbc047435ca8eb10e19d0bbd069bb24b24ae
swilliamson <saffron.williamson@booking.com>
2025-11-07 12:00:13 +0000

    added fields, constructor, getter and deiplay recipe method

--- src/main/java/recipegenerator/Recipe.java
+++ src/main/java/recipegenerator/Recipe.java
@@ -1,5 +1,73 @@
 package recipegenerator;
 
 public class Recipe {
+    private String name;
+    private int spiceLevel;
+    private int serves;
+    private int calories;
+    private int cookingTime;
+    private String[] ingredients;
+    private String instructions;
 
+    //constructor
+    public Recipe(String name, int spiceLevel, int serves, int calories, int cookingTime, String[] ingredients, String instructions) {
+        this.name = name;
+        this.spiceLevel = spiceLevel;
+        this.serves = serves;
+        this.calories = calories;
+        this.cookingTime = cookingTime;
+        this.ingredients = ingredients;
+        this.instructions = instructions;
+    }
+
+    //getters
+    public String getName() {
+        return name;
+    }
+    public int getSpiceLevel() {
+        return spiceLevel;
+    }
+    public int getServes() {
+        return serves;
+    }
+    public int getCalories() {
+        return calories;
+    }
+    public int getCookingTime() {
+        return cookingTime;
+    }
+    public String[] getIngredients() {
+        return ingredients;
+    }
+    public String getInstructions() {
+        return instructions;
+    }
+
+    //display recipe
+    public void displayRecipe() {
+        System.out.println("Recipe: " + name);
+
+        System.out.println("Spice Level: ");
+        if (spiceLevel == 0){
+            System.out.println("Mild \uD83E\uDDCA");
+        } else {
+            for(int i = 0; i < spiceLevel; i++) {
+                System.out.print("\uD83C\uDF36\uFE0F");
+            }
+        }
+
+        System.out.println("Serves: " + serves);
+        System.out.println("Calories: " + calories);
+        System.out.println("Cooking Time: " + cookingTime + " minutes");
+        System.out.println("Ingredients: ");
+        for (String ingredient : ingredients) {
+            System.out.println("- " + ingredient);
+        }
+        System.out.println("Instructions: " + instructions);
+    }
 }
+
+/*
+TODO
+- make recipe look nicely presented
+ */
